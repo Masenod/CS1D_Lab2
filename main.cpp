9a2c88518d9c8cfc878067b393ea789666ce628f
Masenod <64508955+Masenod@users.noreply.github.com>
2021-08-25 16:25:25 -0700

    Update main.cpp
    Changed strings name1 and name2 to our names

--- main.cpp
+++ main.cpp
@@ -8,9 +8,9 @@ int hello1Function(string name1);
 int hello2Function(string name2);
 int main()
 {
-    string name1="your name1";
+    string name1="Mason Emes";
     hello1Function(name1);
-    string name2="your name2";
+    string name2="Yuchen Zhu";
     hello2Function(name2);
     return 0;
 }
